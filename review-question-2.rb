3d83228a6eccb177702468119bd17252a606d341
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-28 09:49:39 -0500

    answered all review questions as a group

--- review-question-2.rb
+++ review-question-2.rb
@@ -1,12 +1,30 @@
 # Finish the implementation of the Car class so it has the functionality described below
-
+require "pry"
 class Car
 
+  attr_accessor :make, :model
+
+  @@all = []
+
+  def initialize(make:, model:)
+    @make = make
+    @model = model
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def drive
+    "VROOOOOOOOOOOOM!"
+  end
 
 
 end
 
-car = Car.new("volvo", "lightening")
+car = Car.new(make: "volvo", model: "lightening")
+
 car.make
 #=> "volvo"
 car.model
